c8ff1306c6a860e561f4c0308c745f922d59ee8d
KarvaMies <jeremias.moberg@gmail.com>
2024-02-09 13:33:12 +0200

    first 2 tasks

---
+++ Coursework/QuickLauncher/app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.quicklauncher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Attempt to launch an activity withing our app
+        Button secondActivityBtn = (Button)findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ButtonClick", "Button clicked");
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("com.example.quicklauncher.SOMETHING", "HELLO WORLD!");
+                startActivity(startIntent);
+            }
+        });
+
+        // Attempt to launch an activity outside our app
+        Button googleBtn = (Button)findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ButtonClick", "Button clicked");
+                String google = "http://www.google.com";
+                Uri webAddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webAddress);
+                if (gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
+            }
+        });
+    }
+}
